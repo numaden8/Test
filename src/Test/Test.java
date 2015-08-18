9bd2b61088bc363c5ffc0c5109ff12eea743a2b5
threepipes <threepipesatos@gmail.com>
2015-08-18 14:10:14 +0900

    九九を追加

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,5 +1,15 @@
 package Test;
 
-public class Test {
+import java.util.Scanner;
 
+public class Test {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		for(int i=1; i<10; i++){
+			for(int j=1; j<10; j++){
+				System.out.print((i*j)+"\t");
+			}
+			System.out.println();
+		}
+	}
 }
